7f2c1319b41f442a75a55bb87160577a8b65c6b6
Joao C Barbosa <joaao@outlook.com>
2021-02-13 14:10:20 -0300

    feat: add DbContext as a service to Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using ContosoPets.Api.Data;
 
 namespace ContosoPets.Api
 {
@@ -27,6 +22,8 @@ namespace ContosoPets.Api
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<ContosoPetsContext>(options =>
+                    options.UseInMemoryDatabase("ContosoPets"));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
